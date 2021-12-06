f4fcc649d880312b1b911f5bdfaa746ac96dfc0c
Agustin-1006 <56725338+Agustin-1006@users.noreply.github.com>
2021-12-06 14:57:28 -0300

    agrego pegada al endpoint de registro

--- src/pages/CrearUsuario.jsx
+++ src/pages/CrearUsuario.jsx
@@ -1,17 +1,49 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
 
 function CrearUsuario() {
-  const [count, setCount] = useState(0);
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [mail, setMail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = { password, name, mail };
+    console.log(user);
+
+    fetch('https://repechaje-backend.herokuapp.com/users', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/JSON' },
+      body: JSON.stringify(user),
+    }).then(() => {
+      console.log('Usuario Creado');
+    });
+  };
+
   return (
     <div>
       <h1>Crear usuario</h1>
-      <form className="form" onSubmit="">
+      <form className="form" onSubmit={handleSubmit}>
         <div>
-          <input type="text" className="form-control" id="name" placeholder="Name" />
-          <input type="email" className="form-control" id="email" placeholder="Email" />
-          <input type="password" className="form-control" id="password" placeholder="Password" />
-          <input type="submit" className="btn btn-primary" value="Crear" onClick={() => setCount(count + 1)} />
-
+          <input
+            type="text"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="mail"
+            required
+            value={mail}
+            onChange={(e) => setMail(e.target.value)}
+          />
+          <input
+            type="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input type="submit" value="Crear usuario" />
         </div>
       </form>
     </div>
