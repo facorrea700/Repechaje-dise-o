7940d1e7dde5a8f9e1c0f4203f160a64260cd940
Facundo Correa <facorrea179@gmail.com>
2021-12-13 13:28:45 -0300

    algo de axios en posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 import { useHistory } from 'react-router-dom';
+import Post from './components/Post';
 
 function App() {
   const history = useHistory();
-
   if (localStorage.getItem('Facebook2_user') === null) {
     history.push('/login');
+  } else {
+    const User = JSON.parse(localStorage.getItem('Facebook2_user'));
+    const userId = User.id.toString();
+    console.log(userId);
+    const pegarle = 'https://repechaje-backend.herokuapp.com/posts';
+    axios.get(pegarle, { headers: { id: ` ${User.id}` } }).then((res) => {
+      console.log(res);
+      if (res.request.status === 200) {
+        // alert('trayendo posts');
+        const post = JSON.stringify(res.data);
+        console.log(post);
+      }
+    });
+    return (
+      <div>
+        <h1>App</h1>
+        <Post />
+        <p>fiumba</p>
+      </div>
+    );
   }
-  return (
-    <div>
-      <h1>App</h1>
-      <p>fiumba</p>
-    </div>
-  );
 }
 
 export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-function Post(props) {
+/* eslint-disable react/prop-types */ // TODO: upgrade to latest eslint tooling
+export default function Post(props) {
+  const { name } = props;
+  console.log(name);
+
   return (
-    <div className="post">
-      <h2>{props.name}</h2>
+    <div className="post-wrapper">
+      <h1>Post</h1>
+      <p>{name}</p>
     </div>
   );
 }
-export default Post;
